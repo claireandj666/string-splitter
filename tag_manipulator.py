a676f803fa765d1fba3536bced0901994b16a62c
claire chen <claire.yandi.chen@gmail.com>
2021-08-03 21:11:56 +0100

    cut3 - step3

--- tag_manipulator.py
+++ tag_manipulator.py
@@ -15,3 +15,13 @@ class TagManipulator():
             return result
 
         return result
+
+class TagManipulator():    
+    def parse_string(self, tags, regex=""):
+        result = []
+
+        tempResult = re.split( regex, tags )
+        if( len(tempResult[0]) > 0 ):
+            result = tempResult  
+
+        return result
